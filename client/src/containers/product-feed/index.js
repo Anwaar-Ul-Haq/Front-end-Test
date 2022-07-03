406d6ae311f013ff846b64cae81d3f0fdb656dda
Anwaar-Ul-Haq <45988297+Anwaar-Ul-Haq@users.noreply.github.com>
2022-07-03 13:51:29 -0400

    implemented logic for inputs and file uploading

--- client/src/containers/product-feed/index.js
+++ client/src/containers/product-feed/index.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
+import EditModal from "../../components/modal";
 import ProductCard from "../../components/product-card";
 
 const ProductFeed = () => {
   const [products, setProducts] = useState([]);
+  const [open, setOpen] = useState(false);
   const [activeIndex, setActiveIndex] = useState(-1);
+
+  const onCloseModal = () => setOpen(false);
 
   const fetchProducts = async () => {
     try {
@@ -24,6 +28,19 @@ const ProductFeed = () => {
     setOpen(true);
   };
 
+  const handleInputChange = (e) => {
+    const updatedProducts = [...products];
+    updatedProducts[activeIndex][e.target.name] = e.target.value;
+    setProducts(updatedProducts);
+  };
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    const updatedProducts = [...products];
+    updatedProducts[activeIndex].image = file;
+    setProducts(updatedProducts);
+  };
+
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-20 mx-10">
       {products?.map((product, index) => {
@@ -35,6 +52,13 @@ const ProductFeed = () => {
           />
         );
       })}
+      <EditModal
+        product={products[activeIndex]}
+        open={open}
+        onCloseModal={onCloseModal}
+        handleInputChange={handleInputChange}
+        handleFileChange={handleFileChange}
+      />
     </div>
   );
 };
